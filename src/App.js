ff86fc3a981fcc6a559f175fad5b37b2ea95fb76
misikira <misikirteka@gmail.com>
2023-08-24 13:48:23 +0300

    add styles codtionally using ternary operator now the packed item is stike out

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Charger", quantity: 1, packed: true },
+  { id: 2, description: "Shoe", quantity: 4, packed: false },
 ];
 export default function App() {
   return ( <div className="app">
@@ -36,7 +38,10 @@ return (
 }
 
 function Item({item}){
-return <li>{item.description}</li>
+return <span style={item.packed?{textDecoration:"line-through"}:{}}>
+<li>{item.description} {item.quantity}</li>
+
+</span> 
 }
 
 function Stats(){
