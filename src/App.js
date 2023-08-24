2664f9e445e3677ca0a3107a22eb163d1ad23ab3
misikira <misikirteka@gmail.com>
2023-08-24 11:53:48 +0300

    add the render featue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 export default function App() {
   return ( <div className="app">
       <Logo/>
@@ -21,7 +24,19 @@ return (
 }
 
 function Itemlist(){
-return <div className="list"></div>
+return (
+  <div className="list">
+  <ul>
+    {initialItems.map((item)=>(
+      <Item item={item}/>
+      ))}
+  </ul>
+  </div>
+);
+}
+
+function Item({item}){
+return <li>{item.description}</li>
 }
 
 function Stats(){
